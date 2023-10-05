0cdc903b3b405c17fbbfe313c23ef7eacd121e51
SergioMerinoCortez <utp0001553@alumno.utpuebla.edu.mx>
2023-10-05 14:54:24 -0600

    piso

--- snake/classes/diorama.ts
+++ snake/classes/diorama.ts
@@ -1,15 +1,17 @@
 import { LifeCycle } from "../types/helpers";
 import { Food } from "./food";
+import { Ground } from "./ground";
 import { Snake } from "./snake";
 
 export class Diorama implements LifeCycle{
 
 private snake: Snake
 private food: Food
-
+private ground: Ground
     public start():void{
 this.snake = new Snake();
 this.food = new Food();
+this.ground = new Ground();
     }
 
 public update():void{
--- snake/classes/ground.ts
+++ snake/classes/ground.ts
@@ -1,7 +1,23 @@
+import { Color, GridHelper } from "three";
 import { LifeCycle } from "../types/helpers";
+import { SceneManager } from "../scene.manager";
 
 export class Ground implements LifeCycle{
+
+private size: number = 15
+private division: number = 15
+public grid :GridHelper
+
+constructor(){
+    this.start();
+}
+
     public start():void{
+
+const color =  new Color(0.1,0.1,0.1);
+this.grid = new GridHelper(this.size, this.division, color, color );
+this.grid.position.set(0,-0.5,0 );
+SceneManager.scene.add(this.grid);
 
     }
 
